764f581ecef55ed3d1dae0d87e81184c04b09551
sonalijarhad <sonalisj.7898@gmail.com>
2019-03-29 15:08:36 +0530

    Secondcommit

--- src/com/sonali/Test.java
+++ src/com/sonali/Test.java
@@ -5,7 +5,18 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("pkg statment");
-
+		// making changes
+		//1
+		///2
+		//3
+		//4
+		//5
+		//6
+		//7
+		//8
+		//9
+		//0
+		System.out.println("Made changes but all are commented");
 	}
 
 }
