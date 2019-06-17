e7ce4c5b8ca1cf3765f6c366d94b985cbc3a35dc
sonalijarhad <sonalisj.7898@gmail.com>
2019-06-17 15:14:24 +0530

    my first change

---
+++ Test.java
@@ -0,0 +1,12 @@
+package com.sonali;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("pkg statment");
+		
+		System.out.println("Made changes but all are commented");
+	}
+
+}
